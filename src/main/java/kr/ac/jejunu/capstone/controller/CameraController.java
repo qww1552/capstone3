1b25c0b54709063444991a8d5ab534dbbf0cb897
qww1552 <qww1552>
2021-04-08 23:42:37 +0900

    before merge

--- src/main/java/kr/ac/jejunu/capstone/controller/CameraController.java
+++ src/main/java/kr/ac/jejunu/capstone/controller/CameraController.java
@@ -2,14 +2,18 @@ package kr.ac.jejunu.capstone.controller;
 
 import kr.ac.jejunu.capstone.Repository.CameraRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 @RestController
-@RequestMapping(value = "/parking/v1/camera")
+@RequestMapping(value = "/parking/v1/cameras")
 public class CameraController {
     @Autowired
     private CameraRepository cameraRepository;
@@ -22,10 +26,13 @@ public class CameraController {
         return body;
     }
 
-    @GetMapping("/cameraId")
-    public String getCameraImage() {
+    @GetMapping(value = "/{cameraId}/image", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ByteArrayOutputStream getCameraImage() throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        String photoUrl = "https://images.unsplash.com/photo-1616663717839-2fea42e1a1f6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=676&q=80";
+        ByteArrayOutputStream cameraImage = restTemplate.getForObject(photoUrl, ByteArrayOutputStream.class);
 
-        return null;
+        return cameraImage;
     }
 
 }
